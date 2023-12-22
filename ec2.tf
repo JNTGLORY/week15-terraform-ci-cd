d56a7f37af6715fb03d5cd91cd00b30566923532
jane <janetntibitaden@gmail.com>
2023-12-22 10:14:05 -0600

    code

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "server1" {
   instance_type = var.instance_type
   ami           = data.aws_ami.ami1.id
   tags = {
-    names = "Dev-server"
+    Name = "Dev-server"
   }
 
 }
