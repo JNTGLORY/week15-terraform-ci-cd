957c0f96260e42e64f0d28d415e42a8e17de90e7
jane <janetntibitaden@gmail.com>
2023-12-22 10:24:55 -0600

    code

--- ec2.tf
+++ ec2.tf
@@ -15,3 +15,11 @@ resource "aws_instance" "server2" {
   }
 
 }
+resource "aws_instance" "server3" {
+  instance_type = var.instance_type
+  ami           = data.aws_ami.ami1.id
+  tags = {
+    Name = "Terraform-server3"
+  }
+
+}
